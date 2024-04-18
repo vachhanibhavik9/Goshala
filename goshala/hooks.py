0002a94f810a686fd12528f2e2f5f402fc5bbd42
vachhanibhavik9 <vachhanibhavik9@gmail.com>
2024-04-18 16:19:15 +0530

    Sales Invoice In Bulk

--- goshala/hooks.py
+++ goshala/hooks.py
@@ -28,7 +28,10 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"Stock Entry" : "public/js/stock_entry.js"}
+doctype_js = {
+    "Stock Entry" : "public/js/stock_entry.js",
+    # "Sales Invoice" : "public/js/sales_invoice.js"
+    }
 # doctype_js = {"doctype" : "public/js/doctype.js"}
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
--- goshala/public/js/stock_entry.js
+++ goshala/public/js/stock_entry.js
@@ -2,7 +2,7 @@
 frappe.ui.form.on('Stock Entry', {
     stock_entry_type(frm) {
         if (frm.doc.stock_entry_type === 'Milk Production') {
-            frm.add_custom_button(__('Get Go'), function() {
+            frm.add_custom_button(__('Get Go'), function () {
                 fetchGoMasterList(frm);
                 fetchGoMasterListForFiltering(frm);
             });
@@ -14,14 +14,13 @@ function fetchGoMasterList(frm) {
     // Make an AJAX call to fetch data from the Go Master doctype
     frappe.call({
         method: 'goshala.goshala.doctype.go_master.go_master.fetch_go_master_list',
-        callback: function(response) {
-            console.log(response.message);
+        callback: function (response) {
             if (response.message) {
                 // Clear existing child table rows
                 frm.clear_table('items');
 
                 // Iterate through the fetched data and add it to the child table
-                response.message.forEach(function(go) {
+                response.message.forEach(function (go) {
                     var row = frappe.model.add_child(frm.doc, 'Stock Entry Detail', 'items');
                     row.custom_go_name = go;
                     // Set other fields as needed
@@ -38,14 +37,13 @@ function fetchGoMasterList(frm) {
 function fetchGoMasterListForFiltering(frm) {
     frappe.call({
         method: 'goshala.goshala.doctype.go_master.go_master.fetch_go_master_list',
-        callback: function(r) {
-            console.log(r.message)
-            if(r.message && r.message.length > 0) {
-                console.log(r.message)
+        callback: function (r) {
+            if (r.message && r.message.length > 0) {
+ 
                 res = r.message
-                
+
                 // Set options for custom_go_name field
-                frm.set_query("custom_go_name", "items", function() {
+                frm.set_query("custom_go_name", "items", function () {
                     return {
                         filters: {
                             'name': ['in', res]
@@ -59,42 +57,41 @@ function fetchGoMasterListForFiltering(frm) {
 
 // Get Customer from Customer where Customer is enabled
 
-// frappe.ui.form.on('Stock Entry', {
-//     stock_entry_type(frm) {
-//         if (frm.doc.stock_entry_type === 'Milk Sales') {
-//             frm.add_custom_button(__('Get Customer'), function() {
-//                 fetch_customer_list(frm);
-//             });
-//         }
-//     }
-// });
+frappe.ui.form.on('Stock Entry', {
+    stock_entry_type(frm) {
+        if (frm.doc.stock_entry_type === 'Milk Sales') {
+            frm.add_custom_button(__('Get Customer'), function() {
+                fetch_customer_list(frm);
+            });
+        }
+    }
+});
 
-// function fetch_customer_list(frm) {
-//     // Make an AJAX call to fetch data from the Go Master doctype
-//     frappe.call({
-//         method: 'goshala.goshala.doctype.go_master.go_master.fetch_customer_list',
-//         callback: function(response) {
-//             console.log(response.message);
-//             if (response.message) {
-//                 // Clear existing child table rows
-//                 frm.clear_table('items');
+function fetch_customer_list(frm) {
+    // Make an AJAX call to fetch data from the Go Master doctype
+    frappe.call({
+        method: 'goshala.goshala.doctype.go_master.go_master.fetch_customer_list',
+        callback: function(response) {
+            if (response.message) {
+                // Clear existing child table rows
+                frm.clear_table('items');
 
-//                 // Iterate through the fetched data and add it to the child table
-//                 response.message.forEach(function(cus) {
-//                     var row = frappe.model.add_child(frm.doc, 'Stock Entry Detail', 'items');
-//                     row.custom_customer_name = cus.name;
-//                     row.custom_pickup = cus.pick_up;
-//                     row.custom_delivery_man = cus.delivery_man;
-//                     row.custom_pickup_counter = cus.pickup_counter;
-//                     row.custom_morning_qty = cus.morning_qty;
-//                     row.custom_evening_qty = cus.evening_qty; 
-//                     console.log(cus);
-//                 });
+                // Iterate through the fetched data and add it to the child table
+                response.message.forEach(function(cus) {
+                    var row = frappe.model.add_child(frm.doc, 'Stock Entry Detail', 'items');
+                    row.custom_customer_name = cus.name;
+                    row.custom_pickup = cus.pick_up;
+                    row.custom_delivery_man = cus.delivery_man;
+                    row.custom_pickup_counter = cus.pickup_counter;
+                    row.custom_morning_qty = parseFloat(cus.morning_qty)? parseFloat(cus.morning_qty):0;
+                    row.custom_evening_qty = parseFloat(cus.evening_qty)? parseFloat(cus.evening_qty):0; 
+                    row.qty = row.custom_morning_qty + row.custom_evening_qty; 
+                });
 
-//                 // Refresh the form to reflect the changes
-//                 frm.refresh_field('items');
-//             }
-//         }
-//     });
-// }
+                // Refresh the form to reflect the changes
+                frm.refresh_field('items');
+            }
+        }
+    });
+}
 
