f387e2baed315f488784b30b2c8bc54d588f059d
vachhanibhavik9 <vachhanibhavik9@gmail.com>
2024-04-26 18:06:37 +0530

    Add transfer or donate detail in go master doc

--- goshala/goshala/doctype/api.py
+++ goshala/goshala/doctype/api.py
@@ -29,7 +29,7 @@ def fetch_go_master_list():
 @frappe.whitelist()
 def fetch_customer_list():
     # Fetch data from the Customer doctype including 'morning_qty' field
-    customer_list = frappe.get_all('Customer', filters={}, fields=['name','custom_morning_qty','custom_evening_qty','custom_pick_up','custom_delivery_man','custom_pickup_counter'])
+    customer_list = frappe.get_all('Customer', filters={'disabled': 0}, fields=['name','custom_morning_qty','custom_evening_qty','custom_pick_up','custom_delivery_man','custom_pickup_counter'])
     return [{'name': cus.name, 'morning_qty': cus.custom_morning_qty, 'evening_qty':cus.custom_evening_qty, 'pick_up':cus.custom_pick_up, 'delivery_man':cus.custom_delivery_man, 'pickup_counter':cus.custom_pickup_counter} for cus in customer_list]
 
 
